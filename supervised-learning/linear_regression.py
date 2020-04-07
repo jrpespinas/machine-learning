e1b0b2873112d316f3d1175403aaaca9894cd29b
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-04-08 01:22:02 +0800

    chore: add new script for linear regression

---
+++ supervised-learning/linear_regression.py
@@ -0,0 +1,16 @@
+"""Linear Regression using Numpy"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__: '1.0.0'
+__author__: 'Jan Rodolf Espinas'
+
+import numpy as np
+
+def main():
+    pass 
+
+if __name__ == '__main__':
+    main()

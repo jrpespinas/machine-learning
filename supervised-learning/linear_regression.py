e611af41ea15cd7fc483399aa0c28606f50c2971
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-04-08 02:23:11 +0800

    feat: use @ instead of np.dot()

--- supervised-learning/linear_regression.py
+++ supervised-learning/linear_regression.py
@@ -9,8 +9,22 @@ __author__: 'Jan Rodolf Espinas'
 
 import numpy as np
 
+# initialize sample data
+data_size = 5 
+theta = np.ones((2,1))
+X = np.column_stack([np.ones(5), np.random.rand(5)])
+
+def hypothesis(X,theta):
+    return X @ theta
+
+def cost_function():
+    pass
+
+def gradient_descent():
+    pass
+
 def main():
-    pass 
+    print(hypothesis(X,theta))
 
 if __name__ == '__main__':
     main()

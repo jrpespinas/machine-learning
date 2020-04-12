45e3f95b2cdc4214e844c59364ea48c9030b7caf
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-04-12 16:43:01 +0800

    refactor(dataset): Use data from sklearn

--- supervised-learning/linear_regression.py
+++ supervised-learning/linear_regression.py
@@ -8,23 +8,25 @@ __version__: '1.0.0'
 __author__: 'Jan Rodolf Espinas'
 
 import numpy as np
+from sklearn.datasets import load_boston
 
-# initialize sample data
-data_size = 5 
-theta = np.ones((2,1))
-X = np.column_stack([np.ones(5), np.random.rand(5)])
+# DATASET
+dataset = load_boston()
+features = dataset.data
+y = dataset.target[:,np.newaxis] 
 
 def hypothesis(X,theta):
     return X @ theta
 
-def cost_function():
+def cost_function(X,y,theta):
     pass
 
 def gradient_descent():
     pass
 
 def main():
-    print(hypothesis(X,theta))
+    print(features)
+    print(y)
 
 if __name__ == '__main__':
     main()

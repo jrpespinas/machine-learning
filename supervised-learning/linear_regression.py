307dc3b0e65b1d7ca3a1e89d930881f8e316f469
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-04-20 01:56:51 +0800

    feat: add data and seed

--- supervised-learning/linear_regression.py
+++ supervised-learning/linear_regression.py
@@ -10,23 +10,39 @@ __author__: 'Jan Rodolf Espinas'
 import numpy as np
 from sklearn.datasets import load_boston
 
+np.random.seed(42)
+
 class LinearRegression():
 
     def __init__(self):
         self.weights = None
-        self.bias = 0
+        self.bias = 1
 
-    def loss(self, X, y, theta):
+    def _loss(self):
         pass
 
     def fit(self, X, y, alpha=0.001, epochs=1000, verbosity=False):
-        pass
+        X = X[:,np.newaxis]
+        m, features = X.shape
+        
+        # initialize weights
+        self.weights = np.random.randn(features,1)
+
+        # hypothesis function
+        hypothesis = self.bias + (X @ self.weights)
+        
 
     def predict(self, X):
         pass
 
 def main():
-    pass
+    dataset = load_boston()
+    features = dataset.data
+    y = dataset.target[:,np.newaxis] 
+    RM = features[:,5] 
+
+    model = LinearRegression()
+    model.fit(RM,y)
 
 if __name__ == '__main__':
     main()

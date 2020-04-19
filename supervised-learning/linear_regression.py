9faa68d707dc00f2f585cc41dbd2850b2b895b46
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-04-20 01:39:38 +0800

    refactor: fix template

--- supervised-learning/linear_regression.py
+++ supervised-learning/linear_regression.py
@@ -12,10 +12,17 @@ from sklearn.datasets import load_boston
 
 class LinearRegression():
 
-    def __init__():
+    def __init__(self):
+        self.weights = None
+        self.bias = 0
+
+    def loss(self, X, y, theta):
         pass
 
-    def fit():
+    def fit(self, X, y, alpha=0.001, epochs=1000, verbosity=False):
+        pass
+
+    def predict(self, X):
         pass
 
 def main():

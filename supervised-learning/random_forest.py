2ff6f08a73cb52641ea61d67d1d7986920cb7b2f
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-04-30 23:55:59 +0800

    chore: add template for random forest

---
+++ supervised-learning/random_forest.py
@@ -0,0 +1,12 @@
+"""Random Forest using Numpy"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__: '1.0.1'
+__author__: 'Jan Rodolf Espinas'
+
+import numpy as np
+
+if __name__ == '__main__':

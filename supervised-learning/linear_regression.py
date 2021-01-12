682f486b7b1ffe19b8d1d4e1dbc95a717ba048b4
jrpespinas <jrpespinas@gmail.com>
2021-01-12 20:30:22 +0800

    feat: add initialize weights

--- supervised-learning/linear_regression.py
+++ supervised-learning/linear_regression.py
@@ -13,7 +13,10 @@ import numpy as np
 class LinearRegression:
     def __init__(self):
         self.weights = None
-        self.bias = None
+        self.bias = 1
+
+    def initialize_weights(self, X):
+        self.weights = np.random.randn(X.shape[1], 1)
 
     def predict(self, X):
         return np.dot(self.weights, X) + self.bias

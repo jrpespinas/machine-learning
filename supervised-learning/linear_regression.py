c31b3256cc38130e54e8fc3ccb8476160d0d6d56
jrpespinas <jrpespinas@gmail.com>
2021-01-12 19:47:22 +0800

    chore: add template for linear regression class

--- supervised-learning/linear_regression.py
+++ supervised-learning/linear_regression.py
@@ -18,113 +18,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '2.0.0'
+__version__ = '3.0.0'
 __author__ = 'Jan Rodolf Espinas'
 
 import numpy as np
-from sklearn.datasets import load_boston
 
 
-np.random.seed(42)
-
-
-class LinearRegression():
-
+class LinearRegression:
     def __init__(self):
-        self.theta = None
-
-    def fit(self, features, y, alpha=0.001, epochs=1000, verbosity=False):
-        '''
-        This function uses gradient descent to minimize the error of the loss
-        function therefore finding the best line that fits through the dataset.
-
-        Parameters
-        ----------
-        features : numpy.ndarray
-            2-dimensional matrix containing the inputs
-        y : numpy.ndarray
-            Nx1 matrix containing the actual values
-        alpha : float
-            Learning rate
-        epochs : int
-            Number of iterations
-        verbosity : bool
-            Display the weights and loss per epoch
-        '''
-
-        # Append vector of `ones` before the first column of features
-        m, n = features.shape
-        X = np.ones((m, n+1))
-        X[:, 1:] = features
-        print(X)
-
-        # initialize the weights
-        theta = np.random.randn(n+1)
-        self.theta = theta[:, np.newaxis]
-
-        # GRADIENT DESCENT
-        for i in range(epochs):
-            y_pred = self.predict(X)
-            self.theta = self.theta - alpha * (1/m) * X.T.dot((y_pred - y))
-
-            # Display weights and loss per epoch
-            if verbosity:
-                if i % 100 == 0:
-                    loss = self.loss(X, y)
-                    print(f'epoch {i}: loss: {loss}, weights: {self.theta}, ')
-
-    def predict(self, X):
-        '''
-        Also known as the hypothesis, this function takes in
-        the input matrix `X` and produces an output.
-
-        Parameters
-        ----------
-        X : numpy.ndarray
-            2-dimensional matrix containing the inputs
-
-        Returns
-        -------
-        numpy.ndarray
-            Matrix containing the outputs
-        '''
-        return np.dot(X, self.theta)
-
-    def loss(self, X, y):
-        '''
-        This function calculates the performance of the model
-
-        Parameters
-        ----------
-        X : numpy.ndarray
-            2-dimensional matrix containing the inputs
-        y : numpy.ndarray
-            Nx1 matrix containing the actual values
-
-        Returns
-        -------
-        float
-            The loss value
-        '''
-        m, _ = X.shape
-        return (1 / (2*m)) * sum(np.square(self.predict(X)-y))
-
-
-def main():
-    dataset = load_boston()
-    features = dataset.data
-    y = dataset.target[:, np.newaxis]
-    RM = features[:, 5]
-    RM = RM[:, np.newaxis]
-    DIS = features[:, 7]
-    DIS = DIS[:, np.newaxis]
-    X = np.ones((DIS.shape[0], 2))
-    X[:, 0:] = RM
-    X[:, 1:] = DIS
-
-    model = LinearRegression()
-    model.fit(RM, y, alpha=0.02, verbosity=1)
-
-
-if __name__ == '__main__':
-    main()
+        self.weights = None
+        self.bias = None

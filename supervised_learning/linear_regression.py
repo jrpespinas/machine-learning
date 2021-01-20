a318fa4ec5b5644f2ddb22653d13a7aabdb6f363
jrpespinas <jrpespinas@gmail.com>
2021-01-20 21:11:25 +0800

    docs: change version number

--- supervised_learning/linear_regression.py
+++ supervised_learning/linear_regression.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '3.0.0'
+__version__ = '0.1.0'
 __author__ = 'Jan Rodolf Espinas'
 
 import numpy as np
--- supervised_learning/logistic_regression.py
+++ supervised_learning/logistic_regression.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = "1.0.0"
+__version__ = "0.1.0"
 __author__ = "Jan Rodolf Espinas"
 
 import numpy as np
@@ -29,7 +29,7 @@ class LogisticRegression(self):
         
         loss = np.multiply(y, np.log(y_hat)) + \
             np.multiply((1 - y), np.log(1 - y_hat))
-        total_loss = np.sum(loss) / m
+        total_loss = - np.sum(loss) / m
 
         total_loss = np.squeeze(total_loss)
 
